949283b27355548abad6c1a8ae4bb130c77be553
reddydodda <reddydodda@gmail.com>
2022-08-05 01:31:22 -0500

    update tools

---
+++ tools/install_dependency.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Install docker and kubectl client 
+
+sudo apt-get update;
+sudo apt install docker.io ca-certificates curl apt-transport-https gnupg software-properties-common jq -y
+sudo usermod -aG docker $USER;
+snap install kubectl --classic
+kubectl version --client
+#sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+#echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#sudo apt-get update
+#sudo apt-get install -y kubectl
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform -y
