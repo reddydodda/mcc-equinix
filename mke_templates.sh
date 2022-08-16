409b7ca04ecafa2ce75f24de7e0cce82a02949e9
reddydodda <reddydodda@gmail.com>
2022-08-16 01:41:35 -0500

    update mke templates

--- mke_templates.sh
+++ mke_templates.sh
@@ -26,8 +26,8 @@ source ${kaas_dir}/variables_mke.sh
 ###################
 
 sed -i "s|EQUINIX_FACILITY|${EQUINIX_FACILITY}|g" $home_dir/cluster.yaml
-sed -i "s|PROJECT_ID|${PROJECT_ID}|g" $home_dir/equinix-config.yaml
-sed -i "s|API_TOKEN|${API_TOKEN}|g" $home_dir/equinix-config.yaml
+sed -i "s||SET_EQUINIX_PROJECT_ID${SET_EQUINIX_PROJECT_ID}|g" $home_dir/equinix-config.yaml
+sed -i "s|SET_EQUINIX_USER_API_TOKEN|${SET_EQUINIX_USER_API_TOKEN}|g" $home_dir/equinix-config.yaml
 sed -i "s|EQUINIX_MACHINE_TYPE_MASTER|${EQUINIX_MACHINE_TYPE_MASTER}|g" $home_dir/machines_master.yaml
 sed -i "s|EQUINIX_MACHINE_TYPE_WORKER|${EQUINIX_MACHINE_TYPE_WORKER}|g" $home_dir/machines_worker.yaml
 
@@ -37,8 +37,21 @@ sed -i "s|CLUSTER_RELEASE|${CLUSTER_RELEASE}|g" $home_dir/cluster.yaml
 sed -i "s|DEDICATED_CONTROL_PLANE|${DEDICATED_CONTROL_PLANE}|g" $home_dir/cluster.yaml
 
 ###################
-# 
+# Networking 
 ###################
+sed -i "s|SET_EQUINIX_VLAN_ID|${SET_EQUINIX_VLAN_ID}|g" $home_dir/cluster.yaml
+sed -i "s|SET_LB_HOST|${SET_LB_HOST}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_METALLB_RANGES|${SET_EQUINIX_METALLB_RANGES}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_NETWORK_CIDR|${SET_EQUINIX_NETWORK_CIDR}|g" $home_dir/*.yaml
+sed -i "s|SET_EQUINIX_NETWORK_GATEWAY|${SET_EQUINIX_NETWORK_GATEWAY}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_NETWORK_DHCP_RANGES|${SET_EQUINIX_NETWORK_DHCP_RANGES}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_CIDR_INCLUDE_RANGES|${SET_EQUINIX_CIDR_INCLUDE_RANGES}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_CIDR_EXCLUDE_RANGES|${SET_EQUINIX_CIDR_EXCLUDE_RANGES}|g" $home_dir/cluster.yaml
+
+
+###############
+# worker nodes
+###############
 count=1
 while [ $count -lt $WORKER_NODES ]; do
 	
@@ -51,12 +64,13 @@ cat << EOF >> $home_dir/machines_worker.yaml
     name: $CLUSTER_NAME-worker-0$count
     namespace: $NAMESPACE
     labels:
-      kaas.mirantis.com/provider: equinixmetal
+      kaas.mirantis.com/provider: equinixmetalv2
       kaas.mirantis.com/region: region-one
       cluster.sigs.k8s.io/cluster-name: $CLUSTER_NAME
   spec: *cp_spec
 EOF
 
+if [ "${CEPH_MANUAL_CONFIGURATION}" = true ]; then
 cat << EOF >> $home_dir/kaascephcluster.yaml
       $CLUSTER_NAME-worker-0$count:
         storageDevices:
@@ -69,6 +83,7 @@ cat << EOF >> $home_dir/kaascephcluster.yaml
             osdsPerDevice: "1"
             deviceClass: ssd
 EOF
+fi
 
 done
 
--- variables_mke.sh
+++ variables_mke.sh
@@ -3,19 +3,39 @@
 ###############
 # variables
 ###############
-export API_TOKEN="<<API_TOKEN>>"
-export PROJECT_ID="<<PROJECT_ID>>"
-export PACKET_TOKEN="<<PACKET_TOKEN>>"
-export EQUINIX_FACILITY=am6
-export EQUINIX_MACHINE_TYPE_MASTER=c3.small.x86
-export EQUINIX_MACHINE_TYPE_WORKER=c3.medium.x86
-export MACHINES_AMOUNT=9
+# Same as METAL_AUTH_TOKEN
+export SET_EQUINIX_USER_API_TOKEN="${METAL_AUTH_TOKEN}"
+# Same as TF_VAR_project_id
+export SET_EQUINIX_PROJECT_ID="${TF_VAR_project_id}"
+# Same as Metro code from step 3
+export EQUINIX_FACILITY=da11
+
+##################
+# Networking
+##################
+export SET_EQUINIX_VLAN_ID="1001"
+export SET_LB_HOST="192.168.1.10"
+export SET_EQUINIX_METALLB_RANGES="192.168.1.30-192.168.1.49"
+export SET_EQUINIX_NETWORK_CIDR="192.168.1.0/24"
+export SET_EQUINIX_NETWORK_GATEWAY="192.168.1.1"
+export SET_EQUINIX_NETWORK_DHCP_RANGES="192.168.1.11-192.168.1.19"
+export SET_EQUINIX_CIDR_INCLUDE_RANGES="192.168.1.20-192.168.1.29"
+export SET_EQUINIX_CIDR_EXCLUDE_RANGES="192.168.1.51-192.168.1.250"
 
 ###
-export CLUSTER_RELEASE=mke-7-2-0-3-4-5
+export CLUSTER_RELEASE=mke-11-3-0-3-5-3
 export CLUSTER_NAME=mke01
 export NAMESPACE=mke
 export DEDICATED_CONTROL_PLANE=true
 
-###
-export WORKER_NODES=5
+#########
+# nodes
+#########
+export EQUINIX_MACHINE_TYPE_MASTER="c3.small.x86"
+export EQUINIX_MACHINE_TYPE_WORKER="c3.small.x86"
+export WORKER_NODES=3
+
+##########
+# ceph
+#########
+export CEPH_MANUAL_CONFIGURATION="False"
