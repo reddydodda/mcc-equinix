e847995ef1795825d9ff6fd383af6ad7e670b214
reddydodda <reddydodda@gmail.com>
2022-08-17 01:02:57 -0500

    add MOS templates for v2

--- mos_templates.sh
+++ mos_templates.sh
@@ -25,20 +25,43 @@ source ${kaas_dir}/variables_mos.sh
 # Update templates
 ###################
 sed -i "s|EQUINIX_FACILITY|${EQUINIX_FACILITY}|g" $home_dir/cluster.yaml
-sed -i "s|PROJECT_ID|${PROJECT_ID}|g" $home_dir/equinix-config.yaml
-sed -i "s|API_TOKEN|${API_TOKEN}|g" $home_dir/equinix-config.yaml
+sed -i "s|SET_EQUINIX_PROJECT_ID|${SET_EQUINIX_PROJECT_ID}|g" $home_dir/equinix-config.yaml
+sed -i "s|SET_EQUINIX_USER_API_TOKEN|${SET_EQUINIX_USER_API_TOKEN}|g" $home_dir/equinix-config.yaml
 sed -i "s|EQUINIX_MACHINE_TYPE_MASTER|${EQUINIX_MACHINE_TYPE_MASTER}|g" $home_dir/machines_master.yaml
-sed -i "s|EQUINIX_MACHINE_TYPE_CTL|${EQUINIX_MACHINE_TYPE_CTL}|g" $home_dir/machines_ctl.yaml
-sed -i "s|EQUINIX_MACHINE_TYPE_CMP|${EQUINIX_MACHINE_TYPE_CMP}|g" $home_dir/machines_cmp.yaml
+sed -i "s|EQUINIX_MACHINE_TYPE_WORKER|${EQUINIX_MACHINE_TYPE_WORKER}|g" $home_dir/machines_worker.yaml
 
 sed -i "s|CLUSTER_NAME|${CLUSTER_NAME}|g" $home_dir/*
 sed -i "s|NAMESPACE|${NAMESPACE}|g" $home_dir/*
 sed -i "s|CLUSTER_RELEASE|${CLUSTER_RELEASE}|g" $home_dir/cluster.yaml
 sed -i "s|DEDICATED_CONTROL_PLANE|${DEDICATED_CONTROL_PLANE}|g" $home_dir/cluster.yaml
 
+sed -i "s|EQUINIX_SSH_KEY_NAME|${EQUINIX_SSH_KEY_NAME}|g" $home_dir/cluster.yaml
+
 ###################
-# 
+# Networking 
 ###################
+sed -i "s|SET_EQUINIX_VLAN_ID|${SET_EQUINIX_VLAN_ID}|g" $home_dir/cluster.yaml
+sed -i "s|SET_LB_HOST|${SET_LB_HOST}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_METALLB_RANGES|${SET_EQUINIX_METALLB_RANGES}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_NETWORK_CIDR|${SET_EQUINIX_NETWORK_CIDR}|g" $home_dir/*.yaml
+sed -i "s|SET_EQUINIX_NETWORK_GATEWAY|${SET_EQUINIX_NETWORK_GATEWAY}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_NETWORK_DHCP_RANGES|${SET_EQUINIX_NETWORK_DHCP_RANGES}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_CIDR_INCLUDE_RANGES|${SET_EQUINIX_CIDR_INCLUDE_RANGES}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_CIDR_EXCLUDE_RANGES|${SET_EQUINIX_CIDR_EXCLUDE_RANGES}|g" $home_dir/cluster.yaml
+sed -i "s|SET_EQUINIX_NETWORK_NAMESERVERS|${SET_EQUINIX_NETWORK_NAMESERVERS}|g" $home_dir/cluster.yaml
+
+################
+# ceph changes
+################
+
+sed -i "s|SET_CEPH_DISK_CLASS|${SET_CEPH_DISK_CLASS}|g" $home_dir/*.yaml
+sed -i "s|CEPH_MANUAL_CONFIGURATION|${CEPH_MANUAL_CONFIGURATION}|g" $home_dir/cluster.yaml
+
+###################
+# Machine changes
+###################
+
+
 count=1
 while [ $count -lt $CMP_NODES ]; do
 	
@@ -51,76 +74,78 @@ cat << EOF >> $home_dir/machines_cmp.yaml
     name: $CLUSTER_NAME-cmp-0$count
     namespace: $NAMESPACE
     labels:
-      kaas.mirantis.com/provider: equinixmetal
+      kaas.mirantis.com/provider: equinixmetalv2
       kaas.mirantis.com/region: region-one
       cluster.sigs.k8s.io/cluster-name: $CLUSTER_NAME
   spec: *cp_spec
 EOF
 
+if [ "${CEPH_MANUAL_CONFIGURATION}" = true ]; then
 cat << EOF >> $home_dir/kaascephcluster.yaml
       $CLUSTER_NAME-cmp-0$count:
         storageDevices:
         - name: sdc
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme0n1
         - name: sdd
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme0n1
         - name: sde
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme0n1
         - name: sdf
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme0n1
         - name: sdg
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme0n1
         - name: sdh
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme0n1
         - name: sdi
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme1n1
         - name: sdj
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme1n1
         - name: sdk
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme1n1
         - name: sdl
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme1n1
         - name: sdm
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme1n1
         - name: sdn
           config:
             osdsPerDevice: "1"
-            deviceClass: hdd
+            deviceClass: ${SET_CEPH_DISK_CLASS}
             metadataDevice: nvme1n1
 EOF
+fi
 
 done
 
--- variables_mos.sh
+++ variables_mos.sh
@@ -3,19 +3,45 @@
 ###############
 # variables
 ###############
-export API_TOKEN="<<API_TOKEN>>"
-export PROJECT_ID="<<PROJECT_ID>>"
-export PACKET_TOKEN="<<PACKET_TOKEN>>"
-export EQUINIX_FACILITY=am6
-export EQUINIX_MACHINE_TYPE_MASTER=c3.small.x86
-export EQUINIX_MACHINE_TYPE_CTL=c3.medium.x86
-export EQUINIX_MACHINE_TYPE_CMP=s3.xlarge.x86
+# Same as METAL_AUTH_TOKEN
+export SET_EQUINIX_USER_API_TOKEN="${METAL_AUTH_TOKEN}"
+# Same as TF_VAR_project_id
+export SET_EQUINIX_PROJECT_ID="${TF_VAR_project_id}"
+# Same as Metro code from step 3
+export EQUINIX_FACILITY=da11
+# Equinx project ssh key
+export EQUINIX_SSH_KEY_NAME="cdodda_key"
 
-###
-export CLUSTER_RELEASE=mos-6-19-0-21-5
+###############
+# Networking
+###############
+export SET_EQUINIX_VLAN_ID="1001"
+export SET_LB_HOST="192.168.1.10"
+export SET_EQUINIX_METALLB_RANGES="192.168.1.30-192.168.1.49"
+export SET_EQUINIX_NETWORK_CIDR="192.168.1.0/24"
+export SET_EQUINIX_NETWORK_GATEWAY="192.168.1.1"
+export SET_EQUINIX_NETWORK_DHCP_RANGES="192.168.1.11-192.168.1.19"
+export SET_EQUINIX_CIDR_INCLUDE_RANGES="192.168.1.20-192.168.1.29"
+export SET_EQUINIX_CIDR_EXCLUDE_RANGES="192.168.1.51-192.168.1.250"
+
+export SET_EQUINIX_NETWORK_NAMESERVERS="192.168.0.1"
+
+##############
+
+export CLUSTER_RELEASE=mosk-8-8-0-22-3
 export CLUSTER_NAME=mostest
 export NAMESPACE=mos
 export DEDICATED_CONTROL_PLANE=true
 
-###
+###############
+# node count
+###############
 export CMP_NODES=3
+export EQUINIX_MACHINE_TYPE_MASTER=c3.small.x86
+export EQUINIX_MACHINE_TYPE_CTL=c3.small.x86
+export EQUINIX_MACHINE_TYPE_CMP=s3.xlarge.x86
+#############
+#ceph
+#############
+export CEPH_MANUAL_CONFIGURATION="True"
+export SET_CEPH_DISK_CLASS="hdd"
